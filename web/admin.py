ffd6f5a943236f92d340b60103e3c9ad392f88c7
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-03-05 00:10:04 -0300

    registrando modelos

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Blog, Blogger, Comment
 
 # Register your models here.
+
+admin.site.register(Blog)
+admin.site.register(Blogger)
+admin.site.register(Comment)
+
