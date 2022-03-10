23876e4b6fe55d86bba56a5bc3cb67b7aa39bc5b
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-03-10 19:36:48 -0300

    criando e modelando campos do site de administracao

--- web/admin.py
+++ web/admin.py
@@ -3,7 +3,16 @@ from .models import Blog, Blogger, Comment
 
 # Register your models here.
 
-admin.site.register(Blog)
-admin.site.register(Blogger)
 admin.site.register(Comment)
 
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'post_date')
+    # fields = ('title', 'author', 'description')
+    readonly_fields = ['comments']
+
+
+@admin.register(Blogger)
+class BloggerAdmin(admin.ModelAdmin):
+    ordering = ('first_name',)
