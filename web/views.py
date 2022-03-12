b9d86198397668b15014eac7d527985832264c5f
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-03-11 21:11:23 -0300

    adicionando a view function 'index'

--- web/views.py
+++ web/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Blog, Blogger, Comment
 
 # Create your views here.
+
+
+def index(request):
+    """
+    Página inicial
+    """
+    
+    # Para contar o número de blogs:
+    num_blogs = Blog.objects.all().count()
+    
+    # Para contar o número de autores:
+    num_bloggers = Blogger.objects.all().count()
+    
+    # Número de visitas para esta view (é contado na variável session):
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    
+    context = {
+        'num_blogs': num_blogs,
+        'num_bloggers': num_bloggers,
+        'num_visits': num_visits
+    }
+    
+    return render(request, 'index.html', context)
