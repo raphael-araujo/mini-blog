1f836a20c3af7cfbe4d6d89e48a6b691e35764d6
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-03-13 00:40:26 -0300

    adicionando a class-based view 'all_blogs'

--- web/urls.py
+++ web/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('all_blogs/', views.BlogsListView.as_view(), name='all_blogs')
 ]
