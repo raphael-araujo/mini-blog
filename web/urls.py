1837b9b672df84026b2c4df738c6b516e6b1a42e
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-03-13 18:10:45 -0300

    adicionando o url para a lista de blogueiros

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('all_blogs/', views.BlogsListView.as_view(), name='all_blogs')
+    path('all_blogs/', views.BlogsListView.as_view(), name='all_blogs'),
+    path('all_bloggers/', views.BloggersListView.as_view(), name='all_bloggers')
 ]
