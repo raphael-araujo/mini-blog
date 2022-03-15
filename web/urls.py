a5973379e2ce0cffca1336cf98654a364df8cc26
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-03-14 22:29:33 -0300

    adicionando o url para a página de detalhe do blog

--- web/urls.py
+++ web/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('all_blogs/', views.BlogsListView.as_view(), name='all_blogs'),
-    path('all_bloggers/', views.BloggersListView.as_view(), name='all_bloggers')
+    path('all_bloggers/', views.BloggersListView.as_view(), name='all_bloggers'),
+    path('<slug:slug>/', views.BlogDetailView.as_view(), name='blog_detail')
 ]
