63b9629ea44659ce560b91e34b88468ee2567472
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-03-16 19:40:04 -0300

    adicionando url para a página de detalhes do blogueiro

--- web/urls.py
+++ web/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('all_blogs/', views.BlogsListView.as_view(), name='all_blogs'),
     path('all_bloggers/', views.BloggersListView.as_view(), name='all_bloggers'),
-    path('<slug:slug>/', views.BlogDetailView.as_view(), name='blog_detail')
+    path('<slug:slug>/', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('blogger/<slug:slug>', views.BloggerDetailView.as_view(), name='blogger_detail')
 ]
