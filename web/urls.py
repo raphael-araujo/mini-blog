e13c8401d2e60c529a0ec810515d490bd8893868
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-04-03 21:51:41 -0300

    adicionando url para a página de criar comentário

--- web/urls.py
+++ web/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('all_blogs/', views.BlogsListView.as_view(), name='all_blogs'),
     path('all_bloggers/', views.BloggersListView.as_view(), name='all_bloggers'),
     path('<slug:slug>/', views.BlogDetailView.as_view(), name='blog_detail'),
-    path('blogger/<slug:slug>', views.BloggerDetailView.as_view(), name='blogger_detail')
+    path('blogger/<slug:slug>', views.BloggerDetailView.as_view(), name='blogger_detail'),
+    path('<slug:slug>/add_comment/', views.comment_page, name='comment_create'),
 ]
