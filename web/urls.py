541708e4f6567ad224d844fd1de9420f2fb227aa
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-04-29 00:10:59 -0300

    adicionando urls para adicionar, editar e remover comentarios

--- web/urls.py
+++ web/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('all_bloggers/', views.BloggersListView.as_view(), name='all_bloggers'),
     path('<slug:slug>/', views.BlogDetailView.as_view(), name='blog_detail'),
     path('blogger/<slug:slug>', views.BloggerDetailView.as_view(), name='blogger_detail'),
-    path('<slug:slug>/add_comment/', views.comment_page, name='comment_create'),
+    path('<slug:slug>/add_commentary/', views.add_commentary, name='add_commentary'),
+    path('<slug:slug>/edit_commentary/', views.edit_commentary, name='edit_commentary'),
+    path('<slug:slug>/remove_commentary/', views.remove_commentary, name='remove_commentary')
 ]
