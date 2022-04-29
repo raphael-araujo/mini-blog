30021307e00a48bfef8c8aeea7d5159158fa15f3
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-04-29 00:07:36 -0300

    criando form para adicionar/editar comentarios

--- web/forms.py
+++ web/forms.py
@@ -3,5 +3,4 @@ from django import forms
 
 class CommentForm(forms.Form):
     """Formulário para adicionar comentários no blog."""
-    comment_date = forms.DateTimeField(required=False, widget=forms.HiddenInput)
     commentary = forms.CharField(max_length=2000, widget=forms.Textarea, help_text='Enter comment about blog here.')
