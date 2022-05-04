4ec4274729232d33f41c7fad966c3b5a8492815f
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-05-03 21:11:50 -0300

    criando a view para a pagina de login

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib import auth , messages
+from .forms import LoginForm
 
 # Create your views here.
+
+
+def login(request):
+    form = LoginForm
+    # form.get_invalid_login_error
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username)
+        print(password)
+        # print(form.errors)
+        user = auth.authenticate(username=username, password=password)
+        
+        if not user:
+            messages.error(request, message="Your username and password didn't match. Please try again.")
+            # print('A')
+            # form.error_messages
+            # form.errors
+            # form.get_invalid_login_error
+            
+            return redirect('/auth/login')
+        
+        else:
+            auth.login(request, user)
+
+            return redirect('/blog/all_blogs')
+
+    else:
+        if request.user.is_authenticated:
+            return redirect('/blog/')
+
+        return render(request, 'login.html', {'form': form})
+
+
+def register(request):
+    return HttpResponse('teste')
