548b4b9b09e77b32e58730680620e2b6aa2f28cb
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-07-01 15:12:38 -0300

    organizando o projeto

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-og4%!@p+fwlc)vw@iaynsyn+&5%dx7cw_)$-7795-hqs10w$q&'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
